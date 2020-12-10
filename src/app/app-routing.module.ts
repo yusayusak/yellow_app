b8f399e7f134571156cd660a0c645d754838465c
Casper Bastiaan-Net <casper.bastiaan.net@gmail.com>
2020-12-10 13:01:23 +0100

    created routing framework

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./index/index.module').then( m => m.IndexPageModule)
   }
 ];
 @NgModule({
---
+++ src/app/measurement/measurement.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-measurement',
+  templateUrl: './measurement.page.html',
+  styleUrls: ['./measurement.page.scss'],
+})
+export class MeasurementPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
