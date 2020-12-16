28122a90494ed416175df683b3f17d7fb353690c
Casper Bastiaan-Net <casper.bastiaan.net@gmail.com>
2020-12-16 21:33:20 +0100

    basic setup for database working

--- src/app/index/index.page.ts
+++ src/app/index/index.page.ts
@@ -3,6 +3,7 @@ import {MeasurementPage} from '../measurement/measurement.page';
 import {IonNav} from '@ionic/angular';
 import {SQLite, SQLiteObject} from '@ionic-native/sqlite/ngx';
 import {DatabaseService} from '../services/database.service';
+import {format} from 'date-fns';
 
 @Component({
   selector: 'app-index',
@@ -13,10 +14,28 @@ export class IndexPage implements OnInit {
   public measurementPage = MeasurementPage;
   public db: SQLiteObject;
   public error = 'nothing yet';
+  public measurements;
 
   constructor(public nav: IonNav, public sqlite: SQLite, public database: DatabaseService) { }
 
   ngOnInit() {
+  }
+
+  public loadData() {
+    this.database.getMeasurements().then((data) => {
+        console.log(data.rows.length);
+        console.log('got the measurements');
+        this.measurements = [];
+        for (let i = 0; i < data.rows.length; i++) {
+          this.measurements.push(data.rows.item(i));
+        }
+        this.error = format(new Date(), 'yyyy-MM-dd HH:mm:ss');
+    });
+  }
+
+  insertData() {
+    this.database.insertMeasurement(0.69, 'an empty string')
+        .finally(() => {console.log('there is something in the database'); });
   }
 
 }
--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import {SQLite, SQLiteObject} from '@ionic-native/sqlite/ngx';
+import getTime from 'date-fns/getTime';
+import {format} from "date-fns";
 
 @Injectable({
   providedIn: 'root'
@@ -10,19 +12,35 @@ export class DatabaseService {
 
   constructor(private sqlite: SQLite) {
     this.createDB();
+    console.log('inserting');
   }
 
   public createDB() {
-    this.sqlite.create({
-      name: 'yellowtest',
-      location: 'default'
+
+     this.sqlite.create({
+         name: 'yellowtest',
+         location: 'default',
+         createFromLocation: 1
     })
         .then((db: SQLiteObject) => {
           this.db = db;
+          this.db.executeSql('DROP TABLE if exists measurements');
           this.db.executeSql(
-              'create table if not exists measurements(id integer(16) PRIMARY KEY AUTOINCREMENT , BAC FLOAT(6, 5), note VARCHAR(255), date TIMESTAMP(32)); ', [])
-              .then(() => console.log('Executed SQL'))
+              'create table if not exists measurements(id INTEGER PRIMARY KEY AUTOINCREMENT , BAC FLOAT(6, 5), note VARCHAR(255), date TIMESTAMP(0)); ', [])
+              .then(() => {
+                  console.log('Executed SQL');
+              })
               .catch(e => console.log(e));
         }).catch(e => console.log(e));
   }
+
+  public insertMeasurement(BAC: number, note = '') {
+      return this.db.executeSql('INSERT INTO measurements (BAC, note, date) values (' + BAC + ', \'' + note + '\', \'' + format(new Date(), 'yyyy-MM-dd HH:mm:ss').toString() + '\')', [])
+          .catch(e => console.log(e));
+      }
+
+    public getMeasurements() {
+      return this.db.executeSql('SELECT * FROM measurements', [])
+          .catch(e => console.log(e));
+    }
 }
